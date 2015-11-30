c3b11200cc1533f65941b82904233381d4239945
kengtang <kengtang@cisco.com>
2015-11-29 21:48:15 -0800

    added popup modal

---
+++ app/assets/javascripts/components/modal.js.jsx
@@ -0,0 +1,31 @@
+let Modal = React.createClass({
+  componentDidMount(){
+    $(this.getDOMNode()).modal('show');
+    $(this.getDOMNode()).on('hidden.bs.modal', this.props.handleHideModal);
+  },
+    render(){
+      return (
+        <div className="modal fade">
+        <div className="modal-dialog">
+        <div className="modal-content">
+        <div className="modal-header">
+        <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 className="modal-title">{this.props.title}</h4>
+        </div>
+        <div className="modal-body">
+          {this.props.content} 
+        </div>
+        <div className="modal-footer">
+        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+        </div>
+        </div>
+        )
+    },
+    propTypes:{
+                handleHideModal: React.PropTypes.func.isRequired
+              }
+});
+
